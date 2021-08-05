46eb0a5bd2556da25a3176757a873684d807e8bf
Rujuta Shinde <rushinde@redhat.com>
2021-08-05 12:30:55 -0400

    exposing pass

--- shell_tox.sh
+++ shell_tox.sh
@@ -1,1 +1,2 @@
+echo "${CONF_SERVER_PASS}"
 tox -e docs-wiki -- -D confluence_space_name="${CONF_SPACE_NAME}" -D confluence_parent_page="${CONF_PARENT_PAGE}" -D confluence_server_user="${CONF_SERVER_USER}" -D confluence_server_pass="${CONF_SERVER_PASS}"
